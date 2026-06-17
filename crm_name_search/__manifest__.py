64b6f189ff1d58b1dcc6b92b423dbd9b54386488
Jickael Agudo <seo@webright.com.au>
2026-06-17 17:19:26 +0800

    Updates

--- crm_name_search/__manifest__.py
+++ crm_name_search/__manifest__.py
@@ -2,14 +2,16 @@
     'name': 'CRM Customer Name Search',
     'version': '1.0',
     'category': 'CRM',
-    'summary': 'Adds separate First Name and Last Name filters to the CRM search bar.',
+    'summary': 'Adds separate First Name and Last Name filters to the CRM and Contact search bars.',
     'author': 'SEOWebright',
     'depends': [
         'crm',
-        'sh_first_last_name'  # <-- This is the crucial missing piece!
+        'contacts',          # Added dependency for the Contacts module
+        'sh_first_last_name' 
     ],
     'data': [
         'views/crm_lead_search_views.xml',
+        'views/res_partner_search_views.xml', # Added the new view file here
     ],
     'installable': True,
     'application': False,
