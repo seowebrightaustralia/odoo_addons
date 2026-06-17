154c831424366364b21a012534be856c2d414f94
Jickael Agudo <seo@webright.com.au>
2026-06-17 16:44:18 +0800

    CRM Name Search

---
+++ crm_name_search/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'CRM Customer Name Search',
+    'version': '1.0',
+    'category': 'CRM',
+    'summary': 'Adds separate First Name and Last Name filters to the CRM search bar.',
+    'author': 'SEOWebright',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_lead_search_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
