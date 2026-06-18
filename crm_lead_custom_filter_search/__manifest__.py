3e683e25c3dd96d19fa42f0aad64f32b2d36e530
Jickael Agudo <seo@webright.com.au>
2026-06-18 17:52:49 +0800

    Updated CRMCFS

---
+++ crm_lead_custom_filter_search/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'CRM Custom Filter Search',
+    'version': '1.0',
+    'category': 'CRM',
+    'summary': 'Extends CRM Lead/Opportunity search to match opportunity name, partner name, sh_firstname, and sh_lastname.',
+    'author': 'SEOWebright',
+    'depends': [
+        'crm',
+        'contacts',
+        'sh_first_last_name',
+    ],
+    'data': [
+        'views/res_partner_search_views.xml',
+        'views/crm_lead_search_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
